13435d1493f8e43b55d5b2b6846930f41fedcf3a
nara yoo <younara3106@gmail.com>
2021-07-12 17:18:47 +0900

    created main slider images and esponsive features

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Carousel } from 'antd';
 import styled,{ keyframes } from 'styled-components';
-// import main2 from '../img/main2 .jpeg';
-// import main4 from '../img/main4.jpeg';
-import main1 from '../img/1.png';
-import main2 from '../img/2.png';
-import main3 from '../img/3.png';
+import main1 from '../img/main1.png';
+import main2 from '../img/main2.png';
+import main3 from '../img/main3.png';
+import ipad1 from '../img/ipad1.png';
+import ipad2 from '../img/ipad2.png';
+import ipad3 from '../img/ipad3.png';
+import iphone1 from '../img/iphone1.png';
+import iphone2 from '../img/iphone2.png';
+import iphone3 from '../img/iphone3.png';
 
 const ani = keyframes`
   0%{
@@ -31,7 +35,14 @@ const Container = styled.div`
     outline: none;
   }
 `; 
+
+
+const Picture = styled.picture`
+
+`
+
 const MainImg = styled.img`
+  width: 100%;
   /* opacity: 0.4; */
 `;
 const MainFont = styled.p`
@@ -58,21 +69,36 @@ function Slider() {
     <CarouselWrapper>
       <Carousel autoplay>
         <Container>
+          <Picture>
+          <source srcSet={main1} media="(min-width: 1000px)" />
+          <source srcSet={ipad1} media="(min-width: 700px)" />
+          <source srcSet={iphone1} media="(min-width: 400px)" />
           <MainImg src={main1}></MainImg>
           {/* <MainFont>나의 기억 속 여행지를 <br />사람들과 공유할 수 있습니다</MainFont> */}
+          </Picture>
         <a href="#" className='scrollAni'><span></span><span></span><span></span></a>
         </Container>
         <Container>
+        <Picture>
+          <source srcSet={main2} media="(min-width: 1000px)" />
+          <source srcSet={ipad2} media="(min-width: 700px)" />
+          <source srcSet={iphone2} media="(min-width: 400px)" />
           <MainImg src={main2}></MainImg>
           {/* <MainFont>Hello, We are Senna {`:)`}<br />Record your memories<br />And share your memories with people</MainFont> */}
         {/* <video controls autoplay="autoplay" loop="loop" muted >
           <source src={main1} type="video/mp4" />
         </video>  */}
+        </Picture>
         <a href="#" className='scrollAni'><span></span><span></span><span></span></a>
         </Container>
         <Container>
+        <Picture>
+          <source srcSet={main3} media="(min-width: 1000px)" />
+          <source srcSet={ipad3} media="(min-width: 700px)" />
+          <source srcSet={iphone3} media="(min-width: 400px)" />
           <MainImg src={main3}></MainImg>
         <a href="#" className='scrollAni'><span></span><span></span><span></span></a>
+        </Picture>
         </Container>
       </Carousel>
     </CarouselWrapper>
